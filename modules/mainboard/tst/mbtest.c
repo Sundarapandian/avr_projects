3461889de5d4a8048e9c6140bb998292d4a559f7
Sundar Andithevar <sundarapandian.andithevar@nxp.com>
2010-03-26 12:25:01 +0000

    ADD: UART Rx Tests

--- modules/mainboard/tst/mbtest.c
+++ modules/mainboard/tst/mbtest.c
@@ -15,17 +15,25 @@ void led_blink_task(void *ignore)
 	}
 }
 
-void uart_test(void * ignore)
+char uart_buf[40]="Hello World\n";
+void uart_tx_test(void * ignore)
 {
-	uart_init();
 	while (1) {
-		uart_puts("Hello World\n");
+		uart_puts(uart_buf);
 		posTaskSleep(MS(5000));
 	}
 }
 
+void uart_rx_test(void * ignore)
+{
+	while (1) {
+		uart_gets(uart_buf);
+	}
+}
 void app_main(void *ignore)
 {
+	uart_init();
 	posTaskCreate(led_blink_task, NULL, 7);
-	posTaskCreate(uart_test, NULL, 6);
+	posTaskCreate(uart_tx_test, NULL, 6);
+	posTaskCreate(uart_rx_test, NULL, 5);
 }
