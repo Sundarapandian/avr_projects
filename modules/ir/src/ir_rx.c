e7cf68cf3230976a0ff0ec0e0ab7b0d81ea5c691
Sundar Andithevar <sundarapandian.andithevar@nxp.com>
2010-05-03 21:14:23 +0000

    Tick timer interrupt service routine added

--- modules/ir/src/ir_rx.c
+++ modules/ir/src/ir_rx.c
@@ -4,10 +4,17 @@ extern POSSEMA_t sync;
 
 static volatile int ticks;
 
+static void tmr_tick(void)
+{
+	ticks ++;
+}
+PICOOS_SIGNAL(SIG_OUTPUT_COMPARE0, tmr_tick);
+
 int ir_rx()
 {
-	/* Start Enable Counter */
-	OCR0 = irp->width;
-	TCCR0 = _BV(WGM01) | _BV(CS01) | _BV(CS00);
+
+	/* Start Ticks Timer */
+	OCR0 = 0xFF;
+	TCCR0 = _BV(WGM01) | _BV(CS01);
 	TIMSK |= _BV(OCIE0);
 }
