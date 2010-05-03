ba132be1ad9176d0f933df6c7b4721e83d22e9ae
Sundar Andithevar <sundarapandian.andithevar@nxp.com>
2010-05-03 09:21:41 +0000

    ADD:Test to power on/off Samsung TV

--- modules/ir/tst/irtest.c
+++ modules/ir/tst/irtest.c
@@ -5,9 +5,10 @@ void app_main(void * ignore)
 {
 	ir_tx_init();
 	while (1) {
-		ir_send_rc5(0x1c, 0x11);
-		posTaskSleep(MS(2000));
-		ir_send_sirc(0x1, 0x13);
-		posTaskSleep(MS(8000));
+		ir_tx(SAMSUNG, 7, 2);
+		//ir_tx(RC5, 0x1c, 0x11);
+		//posTaskSleep(MS(2000));
+		//ir_tx(SIRC, 0x1, 0x13);
+		posTaskSleep(MS(5000));
 	}
 }
